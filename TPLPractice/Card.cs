5ae0cb7bb847d51cc8ef52fa8868cd5f5f108bc6
Шадикьян Руслан Артемович <@corp.itstep>
2019-06-07 17:35:09 +0600

    initial

---
+++ TPLPractice/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPLPractice
+{
+    public class Card
+    {
+        public string Name { get; set; }
+        public string CardSet { get; set; }
+        public string Type { get; set; }
+        public string Rarity { get; set; }
+        public int Cost { get; set; }
+        public int Attack { get; set; }
+        public int Health { get; set; }
+        public string Race { get; set; }
+    }
+}
---
+++ TPLPractice/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace TPLPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task = new Task(() => {
+                WebRequest request = WebRequest.Create("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/Ysera");
+                request.Headers.Add("X-RapidAPI-Host", "omgvamp-hearthstone-v1.p.rapidapi.com");
+                request.Headers.Add("X-RapidAPI-Key", "7be6bad869msh4a1275c8b247fb1p1e2dfdjsna3d2728f7718");
+                WebResponse response = request.GetResponse();
+                using (var stream = response.GetResponseStream())
+                using (var reader = new StreamReader(stream))
+                {
+                    string json = reader.ReadToEnd();
+                    List<Card> card = JsonConvert.DeserializeObject<List<Card>>(json);
+                    Console.WriteLine($"{card[0].Attack} - атака");
+                    Console.WriteLine($"{card[0].Health} - здоровье");
+                    Console.WriteLine($"{card[0].Cost} - цена");
+                    Console.WriteLine($"{card[0].Name} - имя");
+                    Console.WriteLine($"{card[0].Rarity} - редкость");
+                    Console.WriteLine($"{card[0].Type} - тип");
+                    Console.WriteLine($"{card[0].Race} - раса");
+                }
+            });
+            task.Start();
+            Console.ReadLine();
+        }
+    }
+}
